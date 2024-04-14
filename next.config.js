62835720f18fbadb7039547fd6d78135abdfb48d
himanshu-02 <himanshuchougule3430@gmail.com>
2024-04-14 16:55:03 +0530

    add files

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'storage.googleapis.com',
+      'lh1.googleusercontent.com',
+      'lh2.googleusercontent.com',
+      'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
+    ],
+  }
+}
